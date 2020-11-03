8546b2eb07b9cea2bc70a15a1fd7f8e5e7501d1a
Artyom Panfutov <artyom.cox@gmail.com>
2020-11-03 19:44:09 +0300

    new version

--- src/main/java/loan/amortization/lib/domain/EarlyPaymentAdditionalParameters.java
+++ src/main/java/loan/amortization/lib/domain/EarlyPaymentAdditionalParameters.java
@@ -1,8 +1,13 @@
 package loan.amortization.lib.domain;
 
-public enum EarlyPaymentAdditionalParameters {
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.io.Serializable;
+
+public enum EarlyPaymentAdditionalParameters implements Serializable {
     /**
      *  Parameter for {@link EarlyPaymentRepeatingStrategy#TO_CERTAIN_MONTH}
      */
+    @JsonProperty("repeat_to_month_number")
     REPEAT_TO_MONTH_NUMBER
 }
